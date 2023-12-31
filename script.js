6676d6add141994b671af66e773c519fde616dcc
fa635 <bfalonne823@gmail.com>
2023-12-31 14:37:21 +0100

    Add hoover effect

--- script.js
+++ script.js
@@ -9,7 +9,18 @@ const container = document.querySelector("div.main");
 // 16 * 16 grid = 256 squares
 for (let i = 0; i < 256; i++) {
     const square = document.createElement("div");
+    square.setAttribute("class", "square")
     container.appendChild(square);
+
+    //hoover effect
+    square.addEventListener("mouseover", () => {
+        square.style.background = "green";
+    });
+    
+    square.addEventListener("mouseout", () => {
+        square.style.background = "white";
+    });
 }
+
 
 
