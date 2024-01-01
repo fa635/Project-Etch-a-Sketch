acc5497e9fbe5ff97af335fd4fb6fa32119d8554
fa635 <bfalonne823@gmail.com>
2024-01-01 08:29:33 +0100

    Add function for new grid

--- script.js
+++ script.js
@@ -24,3 +24,31 @@ for (let i = 0; i < 256; i++) {
 
 
 
+const button = document.querySelector("button");
+button.addEventListener("click", getNewSquares);
+
+
+function getNewSquares(number) {
+    document.querySelectorAll(".square").forEach(div => div.remove());
+    number = prompt("up to 100");
+    for (let i = 0; i < number * number; i++) {
+        const square = document.createElement("div");
+        square.setAttribute("class", "square")
+        container.appendChild(square);
+    
+        //hoover effect
+        square.addEventListener("mouseover", () => {
+            square.style.background = "green";
+        });
+        
+        square.addEventListener("mouseout", () => {
+            square.style.background = "white";
+        });
+    }
+}
+
+
+
+
+
+
