9545c9e2b73b8b238b8edeab2060e8a24caa56fd
fa635 <bfalonne823@gmail.com>
2024-01-03 06:16:01 +0100

    Darken hoover color 10% more each time

--- script.js
+++ script.js
@@ -58,20 +58,42 @@ function getNewGrid(number) {
         }
         
 
+            
+        
+            //for (let i = 0; i > 0; i -= 10) 
+
+            //getRandomColor().filter = "brightness(" + i + "%)"
+            
+
+
+
+        
+        
+        
+
         //hoover effect
         square.addEventListener("mouseover", () => {
             square.style.background = getRandomColor();
+            square.style.filter = "brightness(" + (x -= 10) + "%)"  
         });
         
         square.addEventListener("mouseout", () => {
             square.style.background = getRandomColor();
+            square.style.filter = "brightness(" + (y -= 10) + "%)"
         });
 
         
+
+        
     }
-
     
+    let x = 100;
+    let y = 100;
 
+    /*for (let x = 100; x > 0; x -= 10) {
+        square.style.filter = "brightness(" + x + "%)"
+    }*/
+    // try to put the hoover and rest outside of the square creation loop
 
     //16 / number of pixel
 }
